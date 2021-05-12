27364c30d922fc1e5bc3f1beb3d2ba02a5d30d6a
Monica Rivera <mriverap@outlook.com>
2021-05-12 16:00:56 -0700

    Initial commit

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string Group { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Notes { get; set; }
+    }
+}
